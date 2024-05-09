6a48f178d54d1399a90a2696c28d878328956190
Marcio Victor <marcio9769@gmail.com>
2024-05-09 18:22:19 -0300

    fix: ordenando os posts pelos mais novos

--- gds/views.py
+++ gds/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from .models import BlogPost
 
 def home(request):
-    posts = BlogPost.objects.all()
+    posts = BlogPost.objects.all().order_by('-created_at')
     return render(request, 'home.html', {'posts': posts})
