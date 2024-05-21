7c9d4dece505104055330685018ccb4559999355
Marcio Victor <marcio9769@gmail.com>
2024-05-21 14:57:26 -0300

    ultimo commit

--- gds/views.py
+++ gds/views.py
@@ -3,4 +3,11 @@ from .models import BlogPost
 
 def home(request):
     posts = BlogPost.objects.all().order_by('-created_at')
-    return render(request, 'home.html', {'posts': posts})
+    showSearch = True
+    return render(request, 'home.html', {'posts': posts, 'showSearch': showSearch})
+
+def gds(request):
+    return render(request, 'gds.html')
+
+def consultoria(request):
+    return render(request, 'consultoria.html')
